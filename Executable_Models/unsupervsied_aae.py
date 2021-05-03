ecbeb87f746d291becfefb6be5399720d3f6325e
phi64702 <philipp.tomac@aisec.fraunhofer.de>
2021-05-03 15:50:26 +0200

    added validation step, not working with gpu

--- Executable_Models/unsupervsied_aae.py
+++ Executable_Models/unsupervsied_aae.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
 import tensorflow as tf
+from loguru import logger
 from lib import models
 
 import time
@@ -37,42 +38,43 @@ output_dir.mkdir(exist_ok=True)
 experiment_dir = output_dir / 'unsupervisied_aae'
 experiment_dir.mkdir(exist_ok=True)
 
-latent_space_dir = experiment_dir / 'latent_space'
+latent_space_dir = experiment_dir / 'experiment_a7n2'
 latent_space_dir.mkdir(exist_ok=True)
 
-# load MNIST dataset
-# TODO: Datensatz MNIST aufbereiten fuer semi-supervised und in Datahandler Klasse auslagern
-# TODO: DataHandler Importieren und Szenaroen für Plots überlegen
-# (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-#
-# x_train = x_train.astype('float32') / 255.
-# x_test = x_test.astype('float32') / 255.
-#
-# # Flatten the dataset
-# x_train = x_train.reshape((-1, 28 * 28))
-# x_test = x_test.reshape((-1, 28 * 28))
+# Data MNIST
+print("Loading and Preprocessing Data with DataHandler.py")
 mnist = MNIST(random_state=random_seed)
-x_train, y_train = mnist.get_alarm_data('train',  list(range(6, 9)), None, list(range(0, 9)))
+
+anomaly = [7]
+drop = [0, 1, 3, 4, 5, 6, 8, 9]
+include = [2, 7]
+
+# Traingins Data
+x_train, y_train = mnist.get_semisupervised_data('train', anomaly, drop, include)
 print(x_train.shape)
 print(y_train.shape)
-x_test, y_test = mnist.get_alarm_data('test',  list(range(6, 9)), None, list(range(0, 9)))
+
+# Testdata
+x_test, y_test = mnist.get_semisupervised_data('test', anomaly, drop, include)
 print(x_test.shape)
 print(y_test.shape)
 
-possible_digits = np.unique(y_test).tolist()
-n_samples = list(y_test)
-print(possible_digits)
-print(n_samples)
-
+# Validation data
+x_val, y_val = mnist.get_semisupervised_data('val', anomaly, drop, include)
+print(x_val.shape)
+print(y_val.shape)
 
 # Parameter
 batch_size = 256
-train_buf = 60000
+train_buf = x_train.shape[0]
 
 train_dataset = tf.data.Dataset.from_tensor_slices(x_train)
 train_dataset = train_dataset.shuffle(buffer_size=train_buf)
 train_dataset = train_dataset.batch(batch_size)
 
+val_dataset = tf.data.Dataset.from_tensor_slices(x_val)
+val_dataset = val_dataset.shuffle(buffer_size=train_buf)
+val_dataset = val_dataset.batch(batch_size)
 
 # Creating the models
 aae = models.AAE()
@@ -84,9 +86,9 @@ encoder = aae.create_encoder()
 decoder = aae.create_decoder()
 discriminator = aae.create_discriminator_style()
 
-encoder.summary()
-decoder.summary()
-discriminator.summary()
+# encoder.summary()
+# decoder.summary()
+# discriminator.summary()
 
 # loss functions
 ae_loss_weight = 1.
@@ -116,11 +118,11 @@ def generator_loss(fake_output, loss_weight):
 base_lr = 0.00025
 max_lr = 0.0025
 
-n_samples = 60000
+n_samples = x_train.shape[0]
 step_size = 2 * np.ceil(n_samples / batch_size)
 global_step = 0
 
-n_epochs = 601
+n_epochs = 300
 
 # Optimizier
 ae_optimizer = tf.keras.optimizers.Adam(lr=base_lr)
@@ -130,6 +132,7 @@ gen_optimizer = tf.keras.optimizers.Adam(lr=base_lr)
 
 # Training
 @tf.function
+@logger.catch
 def train_step(batch_x):
     # Autoencoder
     with tf.GradientTape() as ae_tape:
@@ -171,16 +174,17 @@ def train_step(batch_x):
     gen_grads = gen_tape.gradient(gen_loss, encoder.trainable_variables)
     gen_optimizer.apply_gradients(zip(gen_grads, encoder.trainable_variables))
 
-    return ae_loss, dc_loss, dc_acc, gen_loss
+    return ae_loss, dc_loss, dc_acc, gen_loss, encoder, decoder, discriminator
 
-for epoch in range(n_epochs):
+
+# Start the training
+for epoch in range(n_epochs + 1):
     start = time.time()
 
-    if epoch in [30, 60, 90]:
-        base_lr = base_lr/2
-        max_lr = max_lr/2
-        step_size = step_size/2
-
+    if epoch in [60, 120, 240]:
+        base_lr = base_lr / 2
+        max_lr = max_lr / 2
+        step_size = step_size / 2
 
     epoch_ae_loss_avg = tf.metrics.Mean()
     epoch_dc_loss_avg = tf.metrics.Mean()
@@ -198,7 +202,7 @@ for epoch in range(n_epochs):
         dc_optimizer.lr = clr
         gen_optimizer.lr = clr
 
-        ae_loss, dc_loss, dc_acc, gen_loss = train_step(batch_x)
+        ae_loss, dc_loss, dc_acc, gen_loss, encoder, decoder, discriminator = train_step(batch_x)
 
         epoch_ae_loss_avg(ae_loss)
         epoch_dc_loss_avg(dc_loss)
@@ -234,4 +238,96 @@ for epoch in range(n_epochs):
         ax.set_ylim([-3, 3])
 
         plt.savefig(latent_space_dir / ('epoch_%d.png' % epoch))
+        plt.close('all')
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+# VALIDATION
+@tf.function
+@logger.catch
+def validation(batch_x, encoder_val, decoder_val, discriminator_val):
+    # Autoencoder
+    encoder_output = encoder_val(batch_x, training=False)
+    decoder_output = decoder_val(encoder_output, training=False)
+
+    # Autoencoder loss
+    ae_loss = autoencoder_loss(batch_x, decoder_output, ae_loss_weight)
+
+    # Discriminator
+    real_distribution = tf.random.normal([batch_x.shape[0], z_dim], mean=0.0, stddev=1.0)
+    encoder_output = encoder_val(batch_x, training=False)
+
+    dc_real = discriminator_val(real_distribution, training=False)
+    dc_fake = discriminator_val(encoder_output, training=False)
+
+    # Discriminator Loss
+    dc_loss = discriminator_loss(dc_real, dc_fake, dc_loss_weight)
+
+    # Discriminator Acc
+    dc_acc = accuracy(tf.concat([tf.ones_like(dc_real), tf.zeros_like(dc_fake)], axis=0),
+                          tf.concat([dc_real, dc_fake], axis=0))
+
+    # Generator (Encoder)
+    encoder_output = encoder_val(batch_x, training=False)
+    dc_fake = discriminator_val(encoder_output, training=False)
+
+    # Generator loss
+    gen_loss = generator_loss(dc_fake, gen_loss_weight)
+
+    return ae_loss, dc_loss, dc_acc, gen_loss
+
+
+# Start the training
+for epoch in range(n_epochs + 1):
+    start = time.time()
+
+    epoch_ae_loss_avg = tf.metrics.Mean()
+    epoch_dc_loss_avg = tf.metrics.Mean()
+    epoch_dc_acc_avg = tf.metrics.Mean()
+    epoch_gen_loss_avg = tf.metrics.Mean()
+
+    for batch, (batch_x) in enumerate(val_dataset):
+        # -------------------------------------------------------------------------------------------------------------
+        ae_loss, dc_loss, dc_acc, gen_loss = validation(batch_x,
+                                                        encoder=encoder,
+                                                        decoder=decoder,
+                                                        discriminator=discriminator)
+
+        epoch_ae_loss_avg(ae_loss)
+        epoch_dc_loss_avg(dc_loss)
+        epoch_dc_acc_avg(dc_acc)
+        epoch_gen_loss_avg(gen_loss)
+
+    epoch_time = time.time() - start
+    print('{:4d}: TIME: {:.2f} ETA: {:.2f} AE_LOSS: {:.4f} DC_LOSS: {:.4f} DC_ACC: {:.4f} GEN_LOSS: {:.4f}' \
+          .format(epoch, epoch_time,
+                  epoch_time * (n_epochs - epoch),
+                  epoch_ae_loss_avg.result(),
+                  epoch_dc_loss_avg.result(),
+                  epoch_dc_acc_avg.result(),
+                  epoch_gen_loss_avg.result()))
+
+    if epoch % 100 == 0:
+        # Latent space of test set
+        x_val_encoded = encoder(x_val, training=False)
+        label_list = list(y_val)
+
+        fig = plt.figure()
+        classes = set(label_list)
+        colormap = plt.cm.rainbow(np.linspace(0, 1, len(classes)))
+        kwargs = {'alpha': 0.8, 'c': [colormap[i] for i in label_list]}
+        ax = plt.subplot(111, aspect='equal')
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+        handles = [mpatches.Circle((0, 0), label=class_, color=colormap[i])
+                   for i, class_ in enumerate(classes)]
+        ax.legend(handles=handles, shadow=True, bbox_to_anchor=(1.05, 0.45), fancybox=True, loc='center left')
+        plt.scatter(x_val_encoded[:, 0], x_val_encoded[:, 1], s=2, **kwargs)
+        ax.set_xlim([-3, 3])
+        ax.set_ylim([-3, 3])
+
+        plt.savefig(latent_space_dir / ('validation_epoch_%d.png' % epoch))
         plt.close('all')
