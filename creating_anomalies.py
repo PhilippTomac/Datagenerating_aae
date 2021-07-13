94a9b879fe242615a08bedf6c10955c028bb52dd
phi64702 <philipp.tomac@aisec.fraunhofer.de>
2021-07-13 16:11:46 +0200

    commenting and reviewing the code

--- creating_anomalies.py
+++ creating_anomalies.py
@@ -1,14 +1,9 @@
 ## Imports
 # Matplot
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-import numpy
-from matplotlib import gridspec, colors
-from matplotlib.cm import get_cmap
 
 import numpy as np
 import tensorflow as tf
-import time
 from pathlib import Path
 
 from lib import models
@@ -29,54 +24,81 @@ ROOT_PATH = Path.cwd()
 output_dir = ROOT_PATH / 'Generated_Data/DataPoint_1'
 output_dir.mkdir(exist_ok=True)
 # ------------------------------------------------------------------------------------------------------
+# Setting Seed for better comparison
 random_seed = 1993
 tf.random.set_seed(random_seed)
 np.random.seed(random_seed)
-# ------------------------------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------------------------------
 # Create and load the trained models
 aae = models.AAE()
+'''
+:parameter
+z_dim = 2 - Compression in middle layer
+h_dim = 100 - Denselayer n-neurons
+image_size = 784 
+'''
 z_dim = aae.z_dim
 h_dim = aae.h_dim
 image_size = aae.image_size
 
+# Creating the needed models for the aae
 encoder = aae.create_encoder()
 decoder = aae.create_decoder()
 bad_decoder = aae.create_decoder()
 
-
+# Loading the weights fo the pretrained models in the new encoder and decoder
 encoder.load_weights('/home/fipsi/Documents/Code/Masterarbeit_GPU/TrainedModels/Encoder_6_7/encoder_weights')
 decoder.load_weights('/home/fipsi/Documents/Code/Masterarbeit_GPU/TrainedModels/Decoder_6_7/decoder_weights')
 # ------------------------------------------------------------------------------------------------------
 # Creaing n Datapoints for new Dataset
+'''
+:var
+data -> list with all Datapoints from normal distribution
+images -> list for the generated images
+labels -> list for the labels of the genrated images
+'''
 data = []
 images = []
-dataset = []
 labels = []
 
+# Creating 1000 Datapoints
 for i in range(1000):
+    # look up where the points are in the latent space
+    # set mean and stddev to get the needed information to create the data you need
     z = tf.random.normal([1, 2], mean=3, stddev=3)
     data.append(z)
 
+# Generate images and the labels based on the data in the list data[]
+# set the label of the images you generate
+label = 7
 for i in range(len(data)):
     decoder_out = decoder(data[i])
+    # Convert image-tensor to numpy-array
     decoder_out_numpy = decoder_out.numpy()
+    # Reshape the image to the original shape of 28x28 pixel with values between 0 and 255
     restored_img = decoder_out_numpy.reshape(28, 28, decoder_out_numpy.shape[0]) * 255.
     images.append(restored_img)
-    labels.append(7)
+    labels.append(label)
 
+# convert list to a numpy-array
 labels = np.array(labels)
 
+# ------------------------------------------------------------------------------------------------------
 # Saving the generated Data as new Dataset (datapoints and labels)
+'''
+The images and labels are saved separately in the File DataPoint_x
+x = change this to the label of the data was created
+'''
+
 np.save('/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_7/generated_Images', images)
 np.save('/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_7/generated_labels', labels)
 
-a = np.load('/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_7/generated_Images.npy')
 
-
+# Loading the data and plotting it the 10 first images for review
+test_images = np.load('/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_7/generated_Images.npy')
 for i in range(10):
-    plt.imshow(a[i], cmap='gray')
+    plt.imshow(test_images[i], cmap='gray')
     plt.savefig(output_dir / ('test_generated_loop_%d.png' % i))
     plt.close('all')
 
--- loading_generatedData.py
+++ loading_generatedData.py
@@ -1,62 +1,80 @@
 ## Imports
 # Matplot
-from lib.DataHandler import MNIST
 from typing import List
-
 import numpy as np
 
+# TODO: Add to DataHandler.py
+'''
+Function to generate a new mnist dataset with the the generated data from the aae
+:input
+anomaly_classes ->
+'''
 
-def create_anomalie_dataset(anomaly_classes: List[int] = None):
+def create_anomalie_dataset(needed_classes: List[int] = None):
+    # 1. Load all datasets from every number and the corresponding labels
+
+    # Datapoint 9
     x_train_generated9 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_9/generated_Images.npy')
     y_train_generated9 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_9/generated_labels.npy')
 
+    # Datapoint 8
     x_train_generated8 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_8/generated_Images.npy')
     y_train_generated8 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_8/generated_labels.npy')
 
+    # Datapoint 7
     x_train_generated7 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_7/generated_Images.npy')
     y_train_generated7 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_7/generated_labels.npy')
 
+    # Datapoint 6
     x_train_generated6 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_6/generated_Images.npy')
     y_train_generated6 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_6/generated_labels.npy')
 
+    # Datapoint 5
     x_train_generated5 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_5/generated_Images.npy')
     y_train_generated5 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_5/generated_labels.npy')
 
+    # Datapoint 4
     x_train_generated4 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_4/generated_Images.npy')
     y_train_generated4 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_4/generated_labels.npy')
 
+    # Datapoint 3
     x_train_generated3 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_3/generated_Images.npy')
     y_train_generated3 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_3/generated_labels.npy')
 
+    # Datapoint 2
     x_train_generated2 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_2/generated_Images.npy')
     y_train_generated2 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_2/generated_labels.npy')
 
+    # Datapoint 1
     x_train_generated1 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_1/generated_Images.npy')
     y_train_generated1 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_1/generated_labels.npy')
 
+    # Datapoint 0
     x_train_generated0 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_0/generated_Images.npy')
     y_train_generated0 = np.load(
         '/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/DataPoint_0/generated_labels.npy')
 
+    # 2. Concatenate all arrays into one big dataset
+    #   Datapoints and Label separately
     x_train_generated = np.concatenate([x_train_generated0, x_train_generated1, x_train_generated2, x_train_generated3,
                                         x_train_generated4, x_train_generated5, x_train_generated6, x_train_generated7,
                                         x_train_generated8, x_train_generated9], axis=0)
@@ -65,18 +83,22 @@ def create_anomalie_dataset(anomaly_classes: List[int] = None):
                                         y_train_generated4, y_train_generated5, y_train_generated6, y_train_generated7,
                                         y_train_generated8, y_train_generated9], axis=0)
 
-    x_generated = np.delete(x_train_generated, np.where(np.isin(y_train_generated, anomaly_classes, invert=True)), axis=0)
-    y_generated = np.delete(y_train_generated, np.where(np.isin(y_train_generated, anomaly_classes, invert=True)), axis=0)
-    print(x_generated.shape, y_generated.shape)
+    # 3. Deleting the points that are not wanted in the Dataset
+    #   all classes that are not in the list needed_classes will be deleted from the dataset
+    if needed_classes:
+        x_generated = np.delete(x_train_generated,
+                                np.where(np.isin(y_train_generated, needed_classes, invert=True)), axis=0)
+        y_generated = np.delete(y_train_generated,
+                                np.where(np.isin(y_train_generated, needed_classes, invert=True)), axis=0)
 
+    # 4. Save the created datasets
     np.save('/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/generated_Images', x_generated)
     np.save('/home/fipsi/Documents/Code/Masterarbeit_GPU/Generated_Data/generated_labels', y_generated)
 
+    # 5. Normalising the dataset
+    # TODO: Maybe not needed becouse A3 loads the data and normalise itself. Must be checked!
     x_generated = x_generated.reshape((-1, 28 * 28)) / 255.
 
+    # 6.  return the datasets
     return x_generated, y_generated
-
-a, b = create_anomalie_dataset([6, 7])
-print(a.shape, b.shape)
-print(b)
 
